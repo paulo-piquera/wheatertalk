acdb49c28b7a56d330e645dff1511cdf7cafb1ff
Paulo Piquera <paulo_piquera@hotmail.com>
2023-04-03 21:15:39 -0300

    Weathertalk

---
+++ src/test/kotlin/com/weathertalk/weathertalk/WeathertalkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.weathertalk.weathertalk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WeathertalkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
